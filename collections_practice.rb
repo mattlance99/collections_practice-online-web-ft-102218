26d19489d86a86a085c9e6b69bdcb5f04f7930b0
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-23 12:38:23 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+  a <=> b
+  end
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+  b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+  a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+  a[1] <=> b[2]
+  end
+end
+
+def reverse_array(array)
+  array.reverse do |a, b|
+  a <=> b
+  end
+end
+
+#def kesha_maker(array)
+ # array.each do |char|
+ #   puts "Taking out #{apple}"
+ # end
+#end
+
+def find_a(array)
+  new_array = []
+  array.each do |string|
+  if string.start_with?("a") == true
+    new_array << string
+  end
+  end
+  new_array
+end
+
+def sum_array(array)
+  array.inject{|total, num| total += num; total}
+end
 
